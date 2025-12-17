c08567645c5674ad1232eabc62398f92c5b4e838
Jorge Miranda <jmiranda32691@gmail.com>
2025-12-17 16:21:41 -0600

    had to nest the loop into a function once I realized the rubric required that

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const list = prompt('Enter a list of comma-separated froyo flavors.(example: vanilla bean,coffee,strawberry):');
 // const list = 'berry, apple, pineapple, kiwi, berry, cherry, orange, banana'
-const arr = list.split(',');
+const arr = list.split(','); //builds the array
 
-const trackedFlavors = {};
+const order = orderUp(arr);
 
-for (let i = 0; i < arr.length; i++) {
-    const listedFlavors = arr[i].trim().toUpperCase();
-    if (trackedFlavors[listedFlavors] === undefined) {
-        trackedFlavors[listedFlavors] = 1;
-    } else {
-        trackedFlavors[listedFlavors] += 1;
+function orderUp(arr) {
+    const trackedFlavors = {};
+
+    for (let i = 0; i < arr.length; i++) {
+        const listedFlavors = arr[i].trim().toUpperCase();
+        if (trackedFlavors[listedFlavors] === undefined) {
+            trackedFlavors[listedFlavors] = 1;
+        } else {
+            trackedFlavors[listedFlavors] += 1;
+        }
     }
+    return trackedFlavors;
 }
-
 // console.table(trackedFlavors);
-console.log('Here is what was ordered', trackedFlavors) 
+console.log('Here is what was ordered', order);
 //didn't like how the console displayed the object because 
 // the drop down arrow had to be pressed. 
 // added a string to make it more user friendly.  
