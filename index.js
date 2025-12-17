b227dfeeb5e4f788c2e835fb7a45f6097761ffa8
Jorge Miranda <jmiranda32691@gmail.com>
2025-12-17 16:08:45 -0600

    finished hw

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const list = prompt('Enter a list of comma-separated froyo flavors.')
+const list = prompt('Enter a list of comma-separated froyo flavors.(example: vanilla bean,coffee,strawberry):');
+// const list = 'berry, apple, pineapple, kiwi, berry, cherry, orange, banana'
+const arr = list.split(',');
+
+const trackedFlavors = {};
+
+for (let i = 0; i < arr.length; i++) {
+    const listedFlavors = arr[i].trim().toUpperCase();
+    if (trackedFlavors[listedFlavors] === undefined) {
+        trackedFlavors[listedFlavors] = 1;
+    } else {
+        trackedFlavors[listedFlavors] += 1;
+    }
+}
+
+// console.table(trackedFlavors);
+console.log('Here is what was ordered', trackedFlavors) 
+//didn't like how the console displayed the object because 
+// the drop down arrow had to be pressed. 
+// added a string to make it more user friendly.  
+
